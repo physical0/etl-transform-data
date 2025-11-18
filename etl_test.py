3e7259e400a9f7629949e2d8be4ba9093eb561ee
axel <belaxist@gmail.com>
2025-11-19 05:46:41 +0700

    logging

--- etl_test.py
+++ etl_test.py
@@ -4,8 +4,13 @@ import psycopg2
 from psycopg2.extras import execute_values
 from dotenv import load_dotenv
 import os
+import logging
 
 load_dotenv()
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s',
+                    handlers=[logging.FileHandler("etl_pipeline.log"), logging.StreamHandler()
+                    ])
 
 DB_HOST = os.getenv("DB_HOST")
 DB_NAME = os.getenv("DB_NAME")
@@ -13,28 +18,32 @@ DB_USER = os.getenv("DB_USER")
 DB_PORT = os.getenv("DB_PORT")
 DB_PASS = os.getenv("DB_PASS")
 
-def extract_data():
-    url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd"
-    response = requests.get(url)
-    data = response.json()
-    df = pd.DataFrame(data)
-    return df
+def extract_data(api_url):
+    logging.info("Starting data extraction from API: %s", api_url)
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status()
+        data = response.json()
+        df = pd.DataFrame(data)
+        return df
+    except requests.exceptions.RequestException as e:
+        logging.error("Error extracting data from API: %s", exc_info=True)
+        raise
 
 
 def transform_data(df):
+    try:
+        logging.info("Starting data transformation")
+        df = df[['id', 'name', 'symbol', 'current_price', 'market_cap', 'total_volume', 'last_updated']]
     
-    df = df[['id', 'name', 'symbol', 'current_price', 'market_cap', 'total_volume', 'last_updated']]
+        df = df.dropna()
     
-    df = df.dropna()
+        df['loaded_at'] = pd.Timestamp.now()
     
-    df['loaded_at'] = pd.Timestamp.now()
-    
-    return df
-
-
-df1 = extract_data()
-df2 = transform_data(df1)
-print(df2)
+        return df
+    except Exception as e:
+        logging.error("Error transforming data: %s", exc_info=True)
+        raise
 
 # For checks if env file has been loaded
 print("=== Environment Variables Debug ===")
@@ -47,6 +56,7 @@ print(f"DB_PASS length: {len(os.getenv('DB_PASS') or '')}")
 
 
 def load_pgsql(df):
+    logging.info("Starting data loading to PostgreSQL")
     try:
         conn = psycopg2.connect(
             host = DB_HOST,
@@ -84,12 +94,17 @@ def load_pgsql(df):
         curr.close()
         conn.close()
         
-        print("Data loaded successfully!")
+        logging.info("Data loaded successfully!")
         
     except Exception as e:
-        print(f"Error loading data: {e}")
+        logging.error("Error loading data: %s", e, exc_info=True)
         if 'conn' in locals():
             conn.rollback()
             conn.close()
+        raise
 
+API_URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd"
+df1 = extract_data(API_URL)
+df2 = transform_data(df1)
+print(df2)
 load_pgsql(df2)
